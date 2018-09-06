1d40c36b0c7cd68131e9a5717a1c7253f25cadde
许小六 <>
2018-09-06 10:00:21 +0800

    create commit

---
+++ AddressBook/AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by 许小六 on 2018/9/6.
+//  Copyright © 2018年 许小六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
